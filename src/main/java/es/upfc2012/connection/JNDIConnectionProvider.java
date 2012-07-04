20584617e6e4fabb16503a1bd18ca5c21bcb46c1
spiddy <d.kapanidis@gmail.com>
2012-07-04 14:10:16 +0200

    added jndi

---
+++ src/main/java/es/upfc2012/connection/JNDIConnectionProvider.java
@@ -0,0 +1,27 @@
+/**
+ * $Id$
+ * @author dimitris
+ * @date   Jul 4, 2012 1:53:05 PM
+ *
+ * Copyright (C) 2012 Scytl Secure Electronic Voting SA
+ *
+ * All rights reserved.
+ *
+ */
+package es.upfc2012.connection;
+
+import java.sql.Connection;
+
+/**
+ *
+ */
+public class JNDIConnectionProvider implements ConnectionProvider {
+
+    /**
+     * @see es.upfc2012.connection.ConnectionProvider#getConnection()
+     */
+    public Connection getConnection() {
+        return null;
+    }
+
+}
