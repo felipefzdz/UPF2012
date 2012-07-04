2917ce11dc454af203008290c822bbaf6b22be6b
spiddy <d.kapanidis@gmail.com>
2012-07-04 18:47:20 +0200

    cleanup comments

--- src/main/java/es/upfc2012/connection/MysqlConnectionProvider.java
+++ src/main/java/es/upfc2012/connection/MysqlConnectionProvider.java
@@ -1,13 +1,3 @@
-/**
- * $Id$
- * @author dimitris
- * @date   Jul 4, 2012 1:53:05 PM
- *
- * Copyright (C) 2012 Scytl Secure Electronic Voting SA
- *
- * All rights reserved.
- *
- */
 package es.upfc2012.connection;
 
 import java.sql.Connection;
