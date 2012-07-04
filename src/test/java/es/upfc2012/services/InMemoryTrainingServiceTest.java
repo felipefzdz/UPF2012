fb248892060d6e4b91444d1f2bfcaac02e35d1ce
Ignasi Marimon-Clos <ignasi35@gmail.com>
2012-07-04 14:53:26 +0200

    add retrieve to inmem

--- src/test/java/es/upfc2012/services/InMemoryTrainingServiceTest.java
+++ src/test/java/es/upfc2012/services/InMemoryTrainingServiceTest.java
@@ -1,55 +1,64 @@
 package es.upfc2012.services;
 
+import java.util.List;
+
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import es.upfc2012.domain.Team;
 import es.upfc2012.domain.Training;
 
 public class InMemoryTrainingServiceTest {
 
+	private InMemoryTrainingService _inMemoryTrainingService;
+	private String _teamName;
+	private Training _training;
+
+	@Before
+	public void before() {
+		_teamName = "sample234";
+		String trainingName = "Session in the forest";
+		_training = buildTraining(trainingName, 123);
+		_inMemoryTrainingService = new InMemoryTrainingService();
+	}
+
+	private Training buildTraining(final String trainingName, final int date) {
+		Training training = new Training();
+		training.setName(trainingName);
+		training.setTrainingDate(date);
+		return training;
+	}
+
 	@Test
 	public void testASavedTrainingGetsAnId() throws ServiceException {
-
-		Team team = new Team();
-		team.setName("sample234");
-		Training training = new Training();
-		training.setName("Session in the forest");
-
-		Training save = new InMemoryTrainingService().save(team, training);
+		Training save = _inMemoryTrainingService.save(_teamName, _training);
 		Assert.assertTrue(!save.getId().equals(null));
 	}
-	
+
 	@Test
 	public void testUpdateTrainingThatPrevioslyExist() throws ServiceException {
-
-		Team team = new Team();
-		team.setName("sample234");
-		Training training = new Training();
-		training.setName("Session in the forest");
-		
-		TrainingService inMemory = new InMemoryTrainingService();
-
-		Training toUpdate = inMemory.save(team, training);
+		Training toUpdate = _inMemoryTrainingService.save(_teamName, _training);
 		toUpdate.setName("Session in the Nintendo");
-		Training updated = inMemory.update(team, toUpdate);
-		
+		Training updated = _inMemoryTrainingService.update(_teamName, toUpdate);
 		Assert.assertTrue(updated.getName().equals("Session in the Nintendo"));
 	}
-	
+
 	@Test
-	public void testUpdateTrainingThatPrevioslyNotExist() throws ServiceException {
-
-		Team team = new Team();
-		team.setName("sample234");
-		Training training = new Training();
-		training.setName("Session in the forest");
-		
-		TrainingService inMemory = new InMemoryTrainingService();
-
-		Training updated = inMemory.update(team, training);
-		
+	public void testUpdateTrainingThatPrevioslyNotExist()
+			throws ServiceException {
+		Training updated = _inMemoryTrainingService
+				.update(_teamName, _training);
 		Assert.assertTrue(updated.getName().equals("Session in the forest"));
 	}
+
+	@Test
+	public void testRetrieveReturnsAllTrainings() throws Exception {
+		_inMemoryTrainingService.save(_teamName, _training);
+		_inMemoryTrainingService.save(_teamName, buildTraining("234", 234));
+		_inMemoryTrainingService.save(_teamName, buildTraining("456", 456));
+		List<Training> retrieve = _inMemoryTrainingService.retrieve(_teamName);
+		Assert.assertEquals(3, retrieve.size());
+	}
+
 }
