18831cfee2032f419e172892b3b5c74f107e85a3
spiddy <d.kapanidis@gmail.com>
2012-07-04 17:06:29 +0200

    mysql uses long, not number

--- src/main/resources/db/createDB.sql
+++ src/main/resources/db/createDB.sql
@@ -4,6 +4,6 @@ create table trainings (
 	 id varchar(255), 
 	 team_login varchar(255), 
 	 name varchar(255), 
-	 distance number(10), 
-	 training_date number(19)
+	 distance long,
+	 training_date long
 );
