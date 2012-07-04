f7a8404402a8cbebf01ee03010bad8bc6ad453b3
Felipe Fernández <olid16@gmail.com>
2012-07-04 14:47:56 +0200

    Updating service to new model

--- src/main/java/es/upfc2012/services/DefaultTrainingService.java
+++ src/main/java/es/upfc2012/services/DefaultTrainingService.java
@@ -1,11 +1,13 @@
 package es.upfc2012.services;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import es.upfc2012.connection.ConnectionProvider;
-import es.upfc2012.domain.Team;
 import es.upfc2012.domain.Training;
 
 public class DefaultTrainingService implements TrainingService {
@@ -16,13 +18,15 @@ public class DefaultTrainingService implements TrainingService {
 	private static final String UPDATE_TRAININGS = "update trainings set"
 			+ " team_login = ? , name = ? , distance = ? ,  training_date = ? where id = ?";
 
+	private static final String RETRIEVE_TRAININGS = "select name from trainings where team_login = ? order by desc training_date";
+	
 	private final ConnectionProvider _provider;
 
 	public DefaultTrainingService(final ConnectionProvider provider) {
 		_provider = provider;
 	}
 
-	public Training save(final Team team, final Training training)
+	public Training save(final String teamName, final Training training)
 			throws ServiceException {
 		PreparedStatement stmt;
 		try {
@@ -32,7 +36,7 @@ public class DefaultTrainingService implements TrainingService {
 
 			int index = 0;
 			stmt.setString(index++, training.getId());
-			stmt.setString(index++, team.getName());
+			stmt.setString(index++, teamName);
 			stmt.setString(index++, training.getName());
 			stmt.setLong(index++, training.getDistance());
 			stmt.setLong(index++, training.getTrainingDate());
@@ -45,13 +49,13 @@ public class DefaultTrainingService implements TrainingService {
 
 	}
 
-	public Training update(final Team team, final Training training) throws ServiceException {
+	public Training update(final String teamName, final Training training) throws ServiceException {
 		PreparedStatement stmt;
 		try {
 			stmt = _provider.getConnection().prepareStatement(UPDATE_TRAININGS);
 
 			int index = 0;
-			stmt.setString(index++, team.getName());
+			stmt.setString(index++, teamName);
 			stmt.setString(index++, training.getName());
 			stmt.setLong(index++, training.getDistance());
 			stmt.setLong(index++, training.getTrainingDate());
@@ -64,5 +68,27 @@ public class DefaultTrainingService implements TrainingService {
 			throw new ServiceException(e);
 		}
 
+	}
+
+	@Override
+	public List<Training> retrieve(String teamName) throws ServiceException {
+		PreparedStatement stmt;
+		List<Training> trainings = new ArrayList<Training>();
+		try {
+			stmt = _provider.getConnection().prepareStatement(RETRIEVE_TRAININGS);
+
+			int index = 0;
+			stmt.setString(index++, teamName);
+			ResultSet result = stmt.executeQuery();
+			
+			while(result.next()){
+				Training training = new Training();
+				training.setName(result.getString(1));
+				trainings.add(training);
+			}
+			return trainings;
+		} catch (SQLException e) {
+			throw new ServiceException(e);
+		}
 	}
 }
