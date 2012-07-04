a75bd5ee79b4fff1c804ffe03837dd1974d85ad9
Guillermo <guillermo@guillermo-Aspire-3000.(none)>
2012-07-04 12:49:08 +0200

    Serializer added

--- src/main/java/es/upfc2012/servlets/TeamController.java
+++ src/main/java/es/upfc2012/servlets/TeamController.java
@@ -1,8 +1,50 @@
 package es.upfc2012.servlets;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class RunnerController extends HttpServlet {
+import es.upfc2012.domain.Training;
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
 
+public class TeamController extends HttpServlet {
+
+	//JSONDeserializer<Training> _deserializer = 
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		
+	}
+
+	
+	
+	
 }
  
