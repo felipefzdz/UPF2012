189a043830ced152bc8706fa10106982752dd307
Guillem Mercadal <guillem.mercadal@gmail.com>
2012-07-04 12:37:33 +0200

    Added getters and setters for the team entity.

--- src/main/java/es/upfc2012/domain/Team.java
+++ src/main/java/es/upfc2012/domain/Team.java
@@ -15,5 +15,29 @@ public class Team {
 	private String _name;
 	
 	private List<Training> _trainings;
+
+	public String getId() {
+		return _id;
+	}
+
+	public void setId(String id) {
+		_id = id;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public List<Training> getTrainings() {
+		return _trainings;
+	}
+
+	public void setTrainings(List<Training> trainings) {
+		_trainings = trainings;
+	}
 	
 }
