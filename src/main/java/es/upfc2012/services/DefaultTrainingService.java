7d0fe96666a1b9ec70cffa09935324557680b159
Ignasi Marimon-Clos <ignasi35@gmail.com>
2012-07-04 12:46:53 +0200

    Minimal impl of save training

--- src/main/java/es/upfc2012/services/DefaultTrainingService.java
+++ src/main/java/es/upfc2012/services/DefaultTrainingService.java
@@ -1,5 +1,39 @@
 package es.upfc2012.services;
 
-public class DefaultTrainingService {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import es.upfc2012.connection.ConnectionProvider;
+import es.upfc2012.domain.Team;
+import es.upfc2012.domain.Training;
+
+public class DefaultTrainingService implements TrainingService {
+
+	private static final String INSERT_INTO_TRAININGS = "insert into trainings (id, "
+			+ "team_login, name, distance) values (?,?,?,?)";
+	private final ConnectionProvider _provider;
+
+	public DefaultTrainingService(final ConnectionProvider provider) {
+		_provider = provider;
+	}
+
+	public void save(final Team team, final Training t) {
+		PreparedStatement stmt;
+		try {
+			stmt = _provider.getConnection().prepareStatement(
+					INSERT_INTO_TRAININGS);
+			t.setId(UUID.randomUUID().toString());
+
+			int index = 0;
+			stmt.setString(index++, t.getId());
+			stmt.setString(index++, team.getName());
+			stmt.setString(index++, t.getName());
+			stmt.setString(index++, t.getDistance());
+
+		} catch (SQLException e) {
+		}
+
+	}
 
 }
